be09902e14cfc2dfd8c65169260857c923e760b4
w1nteRR <mykola796@gmail.com>
2025-03-06 15:11:26 +0200

    refactor tracking

--- src/modules/tracking/domain/entities/Tracking.ts
+++ src/modules/tracking/domain/entities/Tracking.ts
@@ -6,44 +6,41 @@ import { RemoveTrackingEvent } from '../events/remove-tracking.event';
 import { CreateTrackingEvent } from '../events/create-tracking.event';
 
 class TrackingCreate {
-  constructor(
-    // public readonly tracking_id: string,
-    public readonly telegram_id: number,
-  ) {}
+  constructor(public readonly user_id: number) {}
 }
 
 export class Tracking extends AggregateRoot<TrackingEvents> {
   constructor(
     private readonly tracking_id: string,
-    private readonly telegram_id: number,
+    private readonly user_id: number,
   ) {
     super();
   }
 
-  get telegramId(): number {
-    return this.telegram_id;
-  }
+  public static create(create: TrackingCreate): Tracking {
+    const trackingId: string = randomUUID();
 
-  get trackingId(): string {
-    return this.tracking_id;
+    const tracking = new Tracking(trackingId, create.user_id);
+
+    tracking.addEvent(
+      new CreateTrackingEvent({
+        tracking_id: trackingId,
+        user_id: create.user_id,
+      }),
+    );
+
+    return tracking;
   }
 
   public remove(): void {
     this.addEvent(new RemoveTrackingEvent({ tracking_id: this.trackingId }));
   }
 
-  public static create(create: TrackingCreate): Tracking {
-    const trackingId: string = randomUUID();
+  get userId(): number {
+    return this.user_id;
+  }
 
-    const tracking = new Tracking(trackingId, create.telegram_id);
-
-    tracking.addEvent(
-      new CreateTrackingEvent({
-        tracking_id: trackingId,
-        telegram_id: create.telegram_id,
-      }),
-    );
-
-    return tracking;
+  get trackingId(): string {
+    return this.tracking_id;
   }
 }
