cf30bc6c2d64440fd5e74fe64e69819b4f763e53
w1nteRR <mykola796@gmail.com>
2025-03-06 15:11:35 +0200

    update tracking repo

--- src/modules/tracking/domain/tracking.repository.ts
+++ src/modules/tracking/domain/tracking.repository.ts
@@ -3,4 +3,5 @@ import { Tracking } from './entities/Tracking';
 export interface ITrackingRepository {
   save(tracking: Tracking): Promise<Tracking>;
   remove(trackingId: string): Promise<void>;
+  findById(trackingId: string): Promise<Tracking | null>;
 }
