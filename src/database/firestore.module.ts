6a7888a3496a15c93b571041966b216cfcbc6b57
w1nteRR <mykola796@gmail.com>
2025-03-20 15:01:01 +0200

    refactor firestore module

--- src/database/firestore.module.ts
+++ src/database/firestore.module.ts
@@ -1,15 +1,26 @@
 import { Module, Global } from '@nestjs/common';
 import { Firestore } from '@google-cloud/firestore';
+import { ConfigService } from '@nestjs/config';
 
 @Global()
 @Module({
   providers: [
     {
+      inject: [ConfigService],
       provide: Firestore,
-      useFactory: () => {
+      useFactory: (configService: ConfigService) => {
+        const projectId = configService.get<string>('GCP_PROJECT_ID');
+        const keyFilename = configService.get<string>('GCP_KEY_FILE');
+
+        if (!projectId || !keyFilename) {
+          throw new Error(
+            'FirestoreModule: GCP_PROJECT_ID or GCP_KEY_FILE not specified.',
+          );
+        }
+
         return new Firestore({
-          projectId: process.env.GCP_PROJECT_ID,
-          keyFilename: process.env.GCP_KEY_FILE,
+          projectId,
+          keyFilename,
         });
       },
     },
