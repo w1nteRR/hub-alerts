a695d2375052b3623b3b7b4a5aeb26726f8aaf81
w1nteRR <mykola796@gmail.com>
2025-03-20 14:58:53 +0200

    add model create & update model domain

--- src/modules/model/domain/entities/Model.ts
+++ src/modules/model/domain/entities/Model.ts
@@ -1,15 +1,38 @@
+import { randomUUID } from 'node:crypto';
 import { Video } from './Video';
 import { AggregateRoot } from '../../../../libs/aggregate-root.base';
 import { ModelEvents } from '../events.enum';
 import { VideoRecentlyUploadedEvent } from '../events/model-video-recently-uploaded.event';
+import { CreateModelEvent } from '../events/create-model.event';
 
 export class Model extends AggregateRoot<ModelEvents> {
   constructor(
+    private readonly model_id: string,
     private readonly name: string,
     private readonly biography: string,
+    private readonly tracking_count: number,
+    private readonly last_recently_uploaded: number,
     public readonly videos: Video[],
   ) {
     super();
+  }
+
+  public static create(create: Model, lastUploadTime: number): Model {
+    const modelId: string = randomUUID();
+    const tracking_count = 1;
+
+    const model = new Model(
+      modelId,
+      create.name,
+      '',
+      tracking_count,
+      lastUploadTime,
+      [],
+    );
+
+    model.addEvent(new CreateModelEvent({ model }));
+
+    return model;
   }
 
   public getNewestVideo(): Video | null {
@@ -30,5 +53,21 @@ export class Model extends AggregateRoot<ModelEvents> {
     if (video.isLastVideoUploadedRecently()) {
       this.addEvent(new VideoRecentlyUploadedEvent({ video }));
     }
+  }
+
+  get modelId(): string {
+    return this.model_id;
+  }
+
+  get modelName(): string {
+    return this.name;
+  }
+
+  get trackingCount(): number {
+    return this.tracking_count;
+  }
+
+  get lastRecentlyUploaded(): number {
+    return this.last_recently_uploaded;
   }
 }
