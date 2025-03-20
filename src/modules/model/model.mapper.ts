8d8cab11fa9daa5f7429e1d6155320df72c9802d
w1nteRR <mykola796@gmail.com>
2025-03-20 14:59:07 +0200

    add firestore map

--- src/modules/model/model.mapper.ts
+++ src/modules/model/model.mapper.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { Mapper } from '../../libs/mapper.base';
 import { ModelPage } from 'pornhub.js';
+
+import { Mapper } from '../../libs/mapper.base';
 import { Model } from './domain/entities/Model';
 import { Video } from './domain/entities/Video';
+import { ModelDocument } from '../../types/model/model.types';
 
 @Injectable()
 export class ModelMapper extends Mapper<ModelPage, Model> {
   mapFrom(source: ModelPage): Model {
     return new Model(
+      '',
       source.name,
       source.bio,
+      0,
+      0,
       source.mostRecentVideos.map(
         ({ id, title, url }) => new Video(id, title, url, 0),
       ),
     );
+  }
+
+  mapToFirestore(source: Model): Partial<ModelDocument> {
+    return {
+      model_id: source.modelId,
+      name: source.modelName,
+      tracking_count: source.trackingCount,
+      last_recently_uploaded: source.lastRecentlyUploaded,
+    };
   }
 }
